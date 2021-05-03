5e24a2761e43bd997bccbd47a948d8f5deb824c0
jdibble21 <jdibble@bsu.edu>
2021-05-03 17:21:55 -0400

    Misc changes

--- project3.py
+++ project3.py
@@ -38,14 +38,15 @@ def predict_using_decision_tree(gender,age,hp,smoke):
             return "no"
     
 def predict_using_probability(gender,age,hp,hd,marry,work,residence,gluc_lvl,bmi,smoke):
-    probability = 0.20
-    
+    probability = 0
     if(age > 55):
-        probability += FACTOR_IMPACT_LARGE
+        probability += FACTOR_IMPACT_MEDIUM
     if(hp == 1):
         probability += FACTOR_IMPACT_LARGE
     if(hd == 1):
         probability += FACTOR_IMPACT_LARGE
+    if(hd == 1 and hp == 0):
+        probability -= FACTOR_IMPACT_LARGE
     if(marry == "No"):
         probability += FACTOR_IMPACT_SMALL
     if(residence == "Rural"):
@@ -56,35 +57,36 @@ def predict_using_probability(gender,age,hp,hd,marry,work,residence,gluc_lvl,bmi
         probability += FACTOR_IMPACT_LARGE
     elif(gluc_lvl <= 199 and gluc_lvl >= 150):
         probability += FACTOR_IMPACT_MEDIUM
-    if(bmi > 25.0):
+    if(bmi > 28.0):
         probability += FACTOR_IMPACT_LARGE
-    if(smoke > "smokes"):
+    elif(bmi <= 28.0 and bmi >25):
+        probability += FACTOR_IMPACT_MEDIUM
+    elif(bmi < 25):
+        probability -= FACTOR_IMPACT_LARGE
+    if(smoke == "smokes"):
         probability += FACTOR_IMPACT_LARGE
+    elif(smoke == "formally smoked"):
+        probability += FACTOR_IMPACT_MEDIUM
+    elif(smoke == "Unknown"):
+        probability += FACTOR_IMPACT_SMALL
     return probability
 
-def determine_probability_risk(prob):
-    pass
+def determine_probability_risk(p):
+    if(p < PROBABILITY_CUTOFF):
+        return "No"
+    if(p >= PROBABILITY_CUTOFF):
+        return "Yes"
 
-def predict_sample_use_cases(df):
-    case_one = df.loc[4219]
-    case_two = df.loc[72911]
-    case_three = df.loc[27419]
-    predict_one = predict_using_decision_tree(case_one.loc['gender'],case_one.loc['age'],case_one.loc['hypertension'],case_one.loc['smoking_status'])
-    predict_two = predict_using_decision_tree(case_two.loc['gender'],case_two.loc['age'],case_two.loc['hypertension'],case_two.loc['smoking_status'])
-    predict_three = predict_using_decision_tree(case_three.loc['gender'],case_three.loc['age'],case_three.loc['hypertension'],case_three.loc['smoking_status'])
-    output_sample_case_data(1,case_one,predict_one)
-    output_sample_case_data(2,case_two,predict_two)
-    output_sample_case_data(3,case_three,predict_three)
-
-    
 def main():
     csv_data = pd.read_csv('data.csv',index_col ="id")
     csv_data.head()
-    ex = csv_data.loc[34120]
-    predict = predict_using_decision_tree(ex.loc['gender'],ex.loc['age'],ex.loc['hypertension'],ex.loc['smoking_status'])
     ids = csv_data.index
-    print(csv_data.describe())
+    ex1 = csv_data.loc[1845]
+    pred1 = predict_using_probability(ex1.loc['gender'],ex1.loc['age'],ex1.loc['hypertension'],ex1.loc['heart_disease'],ex1.loc['ever_married'],ex1.loc['work_type'],ex1.loc['Residence_type'],ex1.loc['avg_glucose_level'],ex1.loc['bmi'],ex1.loc['smoking_status'])
+    print(ex1)
     print("\n")
+    print(pred1)
+    print(determine_probability_risk(pred1))
     #print(ids[0]) save for later: loop through all ids in dataframe
     #print(ex)
     #print(predict)
